1bc587d8025603d011b795356ab22c3bda38e1ce
Royce551 <poohwie2005@gmail.com>
2021-05-15 06:47:00 -0500

    fix crash

--- Water/Containers/GridContainer.cs
+++ Water/Containers/GridContainer.cs
@@ -11,21 +11,21 @@ namespace Water.Containers
 {
     public class GridContainer : GameObject
     {
-        public IContainer[,] Children { get; set; }
+        public IContainer[,] Children { get; set; } = new IContainer[5, 5];
 
         public override Rectangle CalculateLayout()
         {
-            //var currentX = 0;
-            //var currentY = 0;
-            //for (int x = 0; x < Children.Length; x++)
-            //{
-            //    for (int y = 0; y < Children.Length; y++)
-            //    {
-            //        Children[x, y].RelativePosition = new(currentX, currentY, Children[x, y].RelativePosition.Width, Children[x, y].RelativePosition.Height);
-            //        currentX += Children[x, y].RelativePosition.Width;
-            //        currentY += Children[x, y].RelativePosition.Height;
-            //    }
-            //}
+            var currentX = 0;
+            var currentY = 0;
+            for (int x = 0; x < Children.GetLength(0); x++)
+            {
+                for (int y = 0; y < Children.GetLength(1); y++)
+                {
+                    Children[x, y].RelativePosition = new(currentX, currentY, Children[x, y].RelativePosition.Width, Children[x, y].RelativePosition.Height);
+                    currentX += Children[x, y].RelativePosition.Width;
+                    currentY += Children[x, y].RelativePosition.Height;
+                }
+            }
 
             return base.CalculateLayout();
         }
@@ -42,7 +42,7 @@ namespace Water.Containers
 
         public override void Update(GameTime gameTime)
         {
-            //CalculateLayout();
+            CalculateLayout();
         }
     }
 }
