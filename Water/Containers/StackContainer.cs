1256f043df256628ac652183c3fbc0c052d53097
Royce551 <poohwie2005@gmail.com>
2021-05-16 18:18:33 -0500

    implement stack containers

--- Water/Containers/StackContainer.cs
+++ Water/Containers/StackContainer.cs
@@ -1,18 +1,37 @@
-﻿//using Microsoft.Xna.Framework;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Water.Graphics;
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Water.Graphics;
 
-//namespace Water.Containers
-//{
-//    public class StackContainer : Container
-//    {
-//        public override Rectangle CalculateChildrenPositions()
-//        {
-//            return base.CalculateChildrenPositions();
-//        }
-//    }
-//}
+namespace Water.Containers
+{
+    public class StackContainer : Container
+    {
+        public Orientation Orientation { get; set; }
+
+        public override void CalculateChildrenPositions()
+        {
+            int offset;
+            if (Orientation == Orientation.Horizontal) offset = ActualPosition.Width / Children.Count;
+            else offset = ActualPosition.Height / Children.Count;
+
+            int i = 0;
+            foreach (var child in Children)
+            {
+                var newPosition = offset * i;
+                if (Orientation == Orientation.Horizontal) child.ActualPosition = new(ActualPosition.X + newPosition, ActualPosition.Y, offset, ActualPosition.Height);
+                else child.ActualPosition = new(ActualPosition.X, ActualPosition.Y + newPosition, ActualPosition.Width, offset);
+                child.CalculateChildrenPositions();
+                i++;
+            }
+        }
+    }
+    public enum Orientation
+    {
+        Horizontal,
+        Vertical
+    }
+}
